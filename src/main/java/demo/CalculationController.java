13592c47386183c7a6cec613ad7edf66a988b0c3
Stephane Lapie <slapie@slapie-mba.local>
2015-01-26 19:47:48 +0100

    Add service

--- src/main/java/demo/CalculationController.java
+++ src/main/java/demo/CalculationController.java
@@ -3,15 +3,18 @@ package demo;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import javax.validation.Valid;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/calculation")
 public class CalculationController
 {    
-    @RequestMapping("/batch")
+    @RequestMapping(value = "/batch", method = RequestMethod.GET)
     public Collection<CalculationResult> batchCalculation()
     {
     	ArrayList<CalculationResult> results = new ArrayList<CalculationResult>();
@@ -22,8 +25,15 @@ public class CalculationController
     	return results;
     }
     
-    @RequestMapping("/single/{symbol}")
+    @RequestMapping(value = "/single/{symbol}", method = RequestMethod.GET)
     public CalculationResult singleCalculation(@PathVariable String symbol)
+    {
+    	Counterparty ctrp = Counterparties.getCounterparty(symbol);
+    	return new Calculation(ctrp).runFull();
+    }
+    
+    @RequestMapping(value = "/single", method = RequestMethod.POST)
+    public CalculationResult postSingleCalculation(@Valid String symbol)
     {
     	Counterparty ctrp = Counterparties.getCounterparty(symbol);
     	return new Calculation(ctrp).runFull();
